afb079de79531210d909b6be61fe25cadd3ee4cb
kjfdf <kjfdf1@gmail.com>
2020-08-08 23:11:11 +0900

    Update 연습2.R

--- 연습2.R
+++ 연습2.R
@@ -11,3 +11,14 @@ sd2 <- sd(score[group==2])
 effectsize <- abs(mean1-mean2)/(sqrt((sd1^2+sd2^2)/2))
 pwr.t.test(d=effectsize,power=.8,sig.level=.05,type="two.sample",alternative="two.sided")
 # 결과값이 연속변수가 아니거나 정규분포가 아니면 Mann-Whitney U test or Wilcoxon's Rank sum test, 정규분포이고 등분산이 아니면 Welch's T test, 정규분포이고 등분산이면 Student T-test
+library(moonBook)
+data(acs)
+acs
+str(acs)
+summary(acs)
+table(is.na(acs))
+shapiro.test(acs$age[acs$sex=="Male"])
+shapiro.test(acs$age[acs$sex=="Female"])
+var.test(data=acs,age~sex) #p<.05면 등분산성이 아닌것. p>.05이어야 등분산성을 가정할 수 있어서 Student's t-test사용 가능 
+t.test(data=acs,age~sex,var.equal=T) #Student's t test
+t.test(data=acs,age~sex,var.equal=F) #Welch's test
